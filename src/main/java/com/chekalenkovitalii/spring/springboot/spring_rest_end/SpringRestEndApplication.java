f57c4e696a8b76a2b46b42dc59c3f357a2bbfc1e
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-15 18:27:41 +0200

    Spring Boot - App Created

---
+++ src/main/java/com/chekalenkovitalii/spring/springboot/spring_rest_end/SpringRestEndApplication.java
@@ -0,0 +1,13 @@
+package com.chekalenkovitalii.spring.springboot.spring_rest_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chekalenkovitalii/spring/springboot/spring_rest_end/SpringRestEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chekalenkovitalii.spring.springboot.spring_rest_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
